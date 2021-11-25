767516efed637d2739fedd6648aa71812857f5d6
None <None>
2021-11-25 11:28:21 -0800

    changed topo

--- pox_module/cs144/ofhandler.py
+++ pox_module/cs144/ofhandler.py
@@ -191,7 +191,8 @@ def get_ip_setting():
     #print name, ip
     IP_SETTING[name] = ip
 
-  RTABLE.append( ('%s' % IP_SETTING['client'], '%s' % IP_SETTING['client'], '255.255.255.255', 'eth1') )
+  RTABLE.append( ('%s' % IP_SETTING['client1'], '%s' % IP_SETTING['client1'], '255.255.255.255', 'eth1') )
+  RTABLE.append( ('%s' % IP_SETTING['client2'], '%s' % IP_SETTING['client2'], '255.255.255.255', 'eth4') )
   RTABLE.append( ('%s' % IP_SETTING['server1'], '%s' % IP_SETTING['server1'], '255.255.255.255', 'eth2') )
   RTABLE.append( ('%s' % IP_SETTING['server2'], '%s' % IP_SETTING['server2'], '255.255.255.255', 'eth3') )
 
@@ -199,6 +200,7 @@ def get_ip_setting():
   ROUTER_IP['eth1'] = '%s' % IP_SETTING['sw0-eth1']
   ROUTER_IP['eth2'] = '%s' % IP_SETTING['sw0-eth2']
   ROUTER_IP['eth3'] = '%s' % IP_SETTING['sw0-eth3']
+  ROUTER_IP['eth4'] = '%s' % IP_SETTING['sw0-eth4']
   return 0
 
 def launch (transparent=False):
